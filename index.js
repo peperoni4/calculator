c0598955cc030a62f91bba2bfd2ed4d914c79923
peperoni4 <141176443+peperoni4@users.noreply.github.com>
2024-05-15 13:26:09 +0300

    Add basic calculator functionality

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("test message");
+let firstOperand = null;
+let secondOperand = null;
+let operator = null;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(a, b, operator) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) return "ERROR: Division by zero!";
+      return divide(a, b);
+
+    default:
+      return `Unsupported operation - ${operator}!`;
+  }
+}
